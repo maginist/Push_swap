be086f11bd77df40283d509e13cfa00154d190bb
Mathieu GINISTY <maginist@e1r5p16.42.fr>
2019-02-11 13:51:36 +0100

    display almost ready

--- src_c/ft_display.c
+++ src_c/ft_display.c
@@ -6,7 +6,7 @@
 /*   By: maginist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/08 15:32:46 by maginist          #+#    #+#             */
-/*   Updated: 2019/02/08 16:15:29 by maginist         ###   ########.fr       */
+/*   Updated: 2019/02/11 11:33:12 by maginist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,13 @@ void	ft_display(t_stock **a, t_stock **b, int ac, char **av)
 
 	cura = *a;
 	curb = *b;
-	system("clear");
 	if (ac > 2 && av[1][0] == '-' && av[1][1] == 'v')
 	{
+		system("clear");
 		write(1, "           _______  _______ \n", 29);
 		write(1, "          |   A   ||   B   |\n", 29);
 		write(1, "          |-------||-------|\n", 29);
-		while ((cura || cura->next != NULL) || (curb || curb->next != NULL))
+		while (cura || curb)
 		{
 			write(1, "          |", 11);
 			if (cura && sizet(cura) == 1)
@@ -58,59 +58,56 @@ void	ft_display(t_stock **a, t_stock **b, int ac, char **av)
 			else if (cura && sizet(cura) == 2)
 			{
 				write(1, "  ", 2);
-                                ft_putnbr(cura->data);
-                                write(1, "   ||", 5);
+				ft_putnbr(cura->data);
+				write(1, "   ||", 5);
 			}
 			else if (cura && sizet(cura) == 3)
-      			{
-                                write(1, "  ", 2);
-                               	ft_putnbr(cura->data);
-                                write(1, "  ||", 4);
-                        }
+			{
+				write(1, "  ", 2);
+				ft_putnbr(cura->data);
+				write(1, "  ||", 4);
+			}
 			else if (cura && sizet(cura) == 4)
-                        {
-                                write(1, " ", 1);
-                                ft_putnbr(cura->data);
-                                write(1, "  ||", 4);
-                        }
-			else if (sizet(cura) > 4 && sizet(cura) < 1)
+			{
+				write(1, " ", 1);
+				ft_putnbr(cura->data);
+				write(1, "  ||", 4);
+			}
+			else if (cura && sizet(cura) > 4 && sizet(cura) < 1)
 				write(1, "       ||", 9);
 			if (curb && sizet(curb) == 1)
-                        {
-                                write(1, "   ", 3);
-                                ft_putnbr(cura->data);
-                                write(1, "   |\n", 5);
-                        }
+			{
+				write(1, "   ", 3);
+				ft_putnbr(curb->data);
+				write(1, "   |\n", 5);
+			}
 			else if (curb && sizet(curb) == 2)
-                        {
-                                write(1, "  ", 2);
-                                ft_putnbr(cura->data);
-                                write(1, "   |\n", 5);
-                        }
+			{
+				write(1, "  ", 2);
+				ft_putnbr(curb->data);
+				write(1, "   |\n", 5);
+			}
 			else if (curb && sizet(curb) == 3)
-                        {
-                                write(1, "  ", 2);
-                                ft_putnbr(cura->data);
-                                write(1, "  |\n", 4);
-                        }
+			{
+				write(1, "  ", 2);
+				ft_putnbr(curb->data);
+				write(1, "  |\n", 4);
+			}
 			else if (curb && sizet(curb) == 4)
-                        {
-                                write(1, " ", 1);
-                                ft_putnbr(cura->data);
-                                write(1, "  |\n", 4);
-                        }
-			else
+			{
+				write(1, " ", 1);
+				ft_putnbr(curb->data);
+				write(1, "  |\n", 4);
+			}
+			else if (!(curb))
 				write(1, "       |\n", 9);
-			if (cura && cura->next)
+			if (cura)
 				cura = cura->next;
-			if (curb && curb->next)
+			if (curb)
 				curb = curb->next;
-			if ((cura && (cura->next)->data == 0) && (curb && (curb->next)->data == 0))
-				break ;
 		}
 		write(1, "          |_______||_______|\n", 29);
 	}
-
 	else
 		return ;
 }
