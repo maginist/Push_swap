ee33d7baa61745f5a6d13d04dfca1e9353b9adf7
Mathieu GINISTY <maginist@e1r12p8.42.fr>
2019-02-21 18:09:26 +0100

    debut algo

--- src_ps/ft_algoplus.c
+++ src_ps/ft_algoplus.c
@@ -6,13 +6,28 @@
 /*   By: maginist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 09:28:28 by maginist          #+#    #+#             */
-/*   Updated: 2019/02/21 17:57:20 by maginist         ###   ########.fr       */
+/*   Updated: 2019/02/21 18:09:13 by maginist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int		rotate_calcul(t_stock **a, t_stock *cur, t_stock **b)
+void	real_rotate(t_stock **a, t_stock *best, t_stock **b)
+{
+
+}
+
+int		rot_calb(t_stock **a, t_stock *cur, t_stock **b)
+{
+	int		posc;
+	int		size;
+
+	size = size_list(b);
+	posc = bien poser le cur;
+	return (posc);
+}
+
+int		rot_cala(t_stock **a, t_stock *cur, t_stock **b)
 {
 	int		posc;
 	int		size;
@@ -28,23 +43,23 @@ void	ft_algoplus(t_stock **a, t_stock **b)
 {
 	int		res;
 	int		rb;
-	t_stock	*current;
+	t_stock	*cur;
 	t_stock	*best;
 
 	ft_algo3_rev(a, b, 1);
 	best = *a;
 	while (*a)
 	{
-		current = *a;
-		while (current && current->next)
+		cur = *a;
+		while (cur && cur->next)
 		{
-			res = rotate_calcul(a, current, b);
-			rb = rotate_calcul(a, current->next, b);
+			res = rot_cala(a, cur, b) + rot_calb(a, cur, b);
+			rb = rot_cala(a, cur->next, b) + rot_calb(a, cur->next, b);
 			if (res < rb)
-				best = current;
-			current = current->next;
+				best = cur;
+			cur = cur->next;
 		}
-		real_rotate(a, best, b, res);
+		real_rotate(a, best, b);
 		*a = (*a)->next;
 	}
 	while (*b)
